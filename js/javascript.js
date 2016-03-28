876a3b712a88d5be1775b6f8d689336a5bfce94f
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-03-27 22:11:23 -0300

    Arrumando o menu

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,4 @@
 
-$(function() {
-	$(window).scroll(function(){
-		var topo = $('#topo').height(); // altura do topo
-		var rodape = $('#rodape').height(); // altura do rodape
-		var scrollTop = $(window).scrollTop(); // qto foi rolado a barra
-		var tamPagina = $(document).height(); // altura da página
-				 
-		if(scrollTop > topo){
-			$('#menu').css({'position' : 'absolute', 'margin-top' : scrollTop - (topo-5)});
-		}else{
-			$('#menu').css({'position' : 'relative', 'margin-top' : 0});
-		}               
-	});
-});
-
-
 
 function somente_letra(campo){
 	var digits="qwertyuiopasdfghjklçzxcvbnm@"
